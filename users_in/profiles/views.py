c4f5e9ead32a8ba13205ada427622bb1368e847d
Srikar Durgi <sdurgi17@gmail.com>
2015-12-07 10:22:49 +0530

    users_in with couchbase

---
+++ users_in/profiles/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseServerError
+from couchbase.bucket import Bucket
+
+
+def update_profile(request):
+	try:
+		user_dict = request.GET
+		b = Bucket('couchbase://localhost/default')
+		b.upsert(request.GET.get('uid'), user_dict)
+		return HttpResponse("successfully updated user profile")
+	
+	except Exception, e:
+		return HttpResponseServerError("internal server error. Error : {e}".format(e=str(e)))
+	
