2c1e443a59ba10b5ade095993691899601d5397a
Srikar Durgi <sdurgi17@gmail.com>
2015-12-07 18:21:22 +0530

    bad request handled

--- users_us/profiles/views.py
+++ users_us/profiles/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseServerError
+from django.http import HttpResponse, HttpResponseServerError, HttpResponseBadRequest 
 from couchbase.bucket import Bucket
 
 
 def update_profile(request):
 	try:
 		user_dict = request.GET
-		b = Bucket('couchbase://localhost/beer-sample')
-		b.upsert(request.GET.get('uid'), user_dict)
-		return HttpResponse("successfully updated user profile")
+		users_bucket = Bucket('couchbase://localhost/users_us')
+		if 'uid' in user_dict:
+			users_bucket.upsert(request.GET.get('uid'), user_dict)
+			return HttpResponse("successfully updated user profile")
+		else:
+			return HttpResponseBadRequest('uid not found')
 	
 	except Exception, e:
 		return HttpResponseServerError("internal server error. Error : {e}".format(e=str(e)))
